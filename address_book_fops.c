ce9698514c696982cd829417a8d26fe3a89f256e
TastyBishop421 <marcusap64@gmail.com>
2026-03-09 20:38:10 -0700

    save_file implemented

--- address_book_fops.c
+++ address_book_fops.c
@@ -74,12 +74,10 @@ Status load_file(AddressBook *address_book)
 					fclose(address_book->fp);
 					return e_fail;
 				}
-				char *field;
 				if(i == 11)
 				{					
-					field = start; // Last field, take the rest of the line
-					if(isdigit(field[0]))
-						address_book->list[address_book->count].si_no = atoi(field);
+					if(isdigit(start[0]))
+						address_book->list[address_book->count].si_no = atoi(start);
 					else
 					{
 						printf("Invalid si_no: %s\n", buffer);
@@ -92,17 +90,29 @@ Status load_file(AddressBook *address_book)
 					int length = end - start;
 					if(i == 0)
 					{
-						length_check(length, NAME_LEN);
+						if(!length_check(length, NAME_LEN))
+						{
+							fclose(address_book->fp);
+							return e_fail;
+						}
 						strncpy(address_book->list[address_book->count].name[0], start, length);
 					}
 					else if(i >= 1 && i <= 5)
 					{					
-						length_check(length, NUMBER_LEN);
+						if(!length_check(length, NUMBER_LEN))
+						{
+							fclose(address_book->fp);
+							return e_fail;
+						}
 						strncpy(address_book->list[address_book->count].phone_numbers[i - 1], start, length);
 					}
 					else if(i >= 6 && i <= 10)
 					{
-						length_check(length, EMAIL_ID_LEN);
+						if(!length_check(length, EMAIL_ID_LEN))
+						{
+							fclose(address_book->fp);
+							return e_fail;
+						}
 						strncpy(address_book->list[address_book->count].email_addresses[i - 6], start, length);
 					}
 					else
@@ -156,27 +166,95 @@ Status save_file(AddressBook *address_book)
 	 * Write contacts back to file.
 	 * Re write the complete file currently
 	 */ 
+
 	address_book->fp = fopen(DEFAULT_FILE, "w");
 
 	if (address_book->fp == NULL)
 	{
+		printf("Error opening file for writing: %s\n", strerror(errno));
 		return e_fail;
+	}
+
+	for(int i = 0; i < address_book->count; i++)
+	{
+		for(int j = 0; j < 12; j++)
+		{
+			int ret;
+			if(j == 0)
+			{
+				
+				ret = fprintf(address_book->fp, "%s,", address_book->list[i].name[0]);
+				if(!write_error_check(ret))
+				{
+					fclose(address_book->fp);
+					return e_fail;
+				}
+			}
+			else if(j >= 1 && j <= 5)
+			{
+				ret = fprintf(address_book->fp, "%s,", address_book->list[i].phone_numbers[j - 1]);
+				if(!write_error_check(ret))
+				{
+					fclose(address_book->fp);
+					return e_fail;
+				}
+			}
+			else if(j >= 6 && j <= 10)
+			{
+				ret = fprintf(address_book->fp, "%s,", address_book->list[i].email_addresses[j - 6]);
+				if(!write_error_check(ret))
+				{
+					fclose(address_book->fp);
+					return e_fail;
+				}
+			}
+			else if(j == 11)
+			{
+				ret = fprintf(address_book->fp, "%d\n", address_book->list[i].si_no);
+				if(!write_error_check(ret))
+				{
+					fclose(address_book->fp);
+					return e_fail;
+				}
+			}
+			else 
+			{
+				printf("Unexpected field index: %d\n", j);
+				fclose(address_book->fp);
+				return e_fail;
+			}
+		}
 	}
 
 	/* 
 	 * Add the logic to save the file
 	 * Make sure to do error handling
 	 */ 
-	fclose(address_book->fp);
+	if(fclose(address_book->fp) < 0)
+	{
+		printf("Error closing file: %s\n", strerror(errno));
+		return e_fail;
+	}
 
 	return e_success;
 }
 
-void length_check(int len, int max_len)
+bool length_check(int len, int max_len)
 {
 	if(len >= max_len)
 	{
 		printf("Input exceeds maximum length of %d characters.\n", max_len - 1);
-		exit(EXIT_FAILURE);
+		return false;
 	}
+	return true;
+}
+
+bool write_error_check(int ret)
+{
+	if(ret < 0)
+	{
+		printf("Error writing to file: %s\n", strerror(errno));
+		return false;
+	}
+	return true;
 }
